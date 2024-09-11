0f7e4950bba4aad302086a93ccea1d288b66eda0
kylin <260987762@qq.com>
2024-09-11 18:16:29 +0800

    添加组件

---
+++ amis/data_input/Input_date_time.py
@@ -0,0 +1,75 @@
+from typing import Optional, Union
+
+from amis.data_input import FormItem
+
+
+class InputDatetime(FormItem):
+    """
+    InputDatetime 日期时间
+
+    参考：https://aisuda.bce.baidu.com/amis/zh-CN/components/form/input-datetime#%E5%B1%9E%E6%80%A7%E8%A1%A8
+    """
+
+    type: str = "input-datetime"
+    """指明为 input-datetime 组件"""
+    value: Optional[str] = None
+    """默认值"""
+    valueFormat: Optional[str] = None
+    """
+    - 日期时间选择器值格式，更多格式类型请参考 文档
+    - 默认值：'X'
+    - 版本：3.4.0 版本后支持
+    """
+    displayFormat: Optional[str] = None
+    """
+    - 日期时间选择器显示格式，即时间戳格式，更多格式类型请参考 文档
+    - 默认值：YYYY-MM-DD HH:mm:ss
+    - 版本：3.4.0 版本后支持
+    """
+    placeholder: Optional[str] = None
+    """
+    - 占位文本
+    - 默认值：'请选择日期以及时间'
+    - 版本：3.4.0 版本后支持
+    """
+    shortcuts: Optional[Union[str, list[str], list[dict]]] = None
+    """
+    - 日期时间快捷键
+    - 版本：3.1.0 版本后支持表达式
+    """
+    minDate: Optional[str] = None
+    """限制最小日期时间"""
+    maxDate: Optional[str] = None
+    """限制最大日期时间"""
+    utc: Optional[bool] = None
+    """
+    - 保存 utc 值
+    - 默认值：false
+    """
+    clearable: Optional[bool] = None
+    """
+    - 是否可清除
+    - 默认值：true
+    """
+    embed: Optional[bool] = None
+    """
+    - 是否内联
+    - 默认值：false
+    """
+    timeConstraints: Optional[dict] = None
+    """
+    - 请参考 input-time 里的说明
+    - 默认值：true
+    """
+    isEndDate: Optional[dict] = None
+    """
+    - 如果配置为 true，会自动默认为 23:59:59 秒
+    - 默认值：false
+    """
+    disabledDate: Optional[str] = None
+    """用字符函数来控制哪些天不可以被点选"""
+    popOverContainerSelector: Optional[str] = None
+    """
+    - 弹层挂载位置选择器，会通过querySelector获取
+    - 版本：6.4.0 版本后支持
+    """
---
+++ amis/data_input/combo.py
@@ -0,0 +1,136 @@
+from typing import Optional
+from pydantic import SerializeAsAny
+from amis.data_input import FormItem
+from amis.function.button import Button
+from amis.types import *
+
+
+class Combo(FormItem):
+    """
+    Combo 组合
+
+    用于将多个表单项组合到一起，实现深层结构的数据编辑。
+
+    比如想提交 user.name 这样的数据结构，有两种方法：一种是将表单项的 name 设置为user.name，另一种就是使用 combo。
+
+    参考：https://aisuda.bce.baidu.com/amis/zh-CN/components/form/combo#%E5%B1%9E%E6%80%A7%E8%A1%A8
+    """
+
+    type: str = "combo"
+    """指定为 combo 组件"""
+    formClassName: Optional[str] = None
+    """单组表单项的类名"""
+    items: SerializeAsAny[Optional[list[FormItem]]] = None
+    """组合展示的表单项"""
+    noBorder: Optional[bool] = None
+    """
+    - 单组表单项是否显示边框
+    - 默认值：false
+    """
+    scaffold: Optional[dict] = None
+    """单组表单项初始值"""
+    multiple: Optional[bool] = False
+    """是否多选"""
+    multiLine: Optional[bool] = False
+    """默认是横着展示一排，设置以后竖着展示"""
+    minLength: Optional[int] = None
+    """
+    - 最少添加的条数
+    - 版本: 2.4.1 版本后支持变量
+    """
+    maxLength: Optional[int] = None
+    """
+    - 最多添加的条数
+    - 版本: 2.4.1 版本后支持变量
+    """
+    flat: Optional[bool] = False
+    """
+    - 是否将结果扁平化(去掉 name),只有当 items 的 length 为 1 且 multiple 为 true 的时候才有效
+    - 默认值：false
+    """
+    joinValues: Optional[bool] = True
+    """
+    - 默认为 true 当扁平化开启的时候，是否用分隔符的形式发送给后端，否则采用 array 的方式
+    - 默认值：true
+    """
+    delimiter: Optional[str] = None
+    """当扁平化开启并且 joinValues 为 true 时，用什么分隔符"""
+    addable: Optional[bool] = False
+    """
+    - 是否可新增
+    - 默认值：false
+    """
+    addattop: Optional[bool] = False
+    """
+    - 在顶部添加
+    - 默认值：false
+    """
+    removable: Optional[bool] = False
+    """
+    - 是否可删除
+    - 默认值：false
+    """
+    deleteApi: Optional[API] = None
+    """如果配置了，则删除前会发送一个 api，请求成功才完成删除"""
+    deleteConfirmText: Optional[str] = None
+    """当配置 deleteApi 才生效！删除时用来做用户确认"""
+    draggable: Optional[bool] = False
+    """
+    - 是否可以拖动排序, 需要注意的是当启用拖动排序的时候，会多一个$id 字段
+    - 默认值：false
+    """
+    draggableTip: Optional[str] = None
+    """可拖拽的提示文字"""
+    subFormMode: Optional[Literal['normal', 'horizontal', 'inline']] = None
+    """
+    - 子表单模式
+    - 默认值：'normal'
+    """
+    subFormHorizontal: Optional[dict] = None
+    """
+    - 当 subFormMode 为 horizontal 时有用，用来控制 label 的展示占比
+    - 默认值：false
+    """
+    placeholder: Optional[str] = None
+    """
+    - 没有成员时显示
+    """
+    canAccessSuperData: Optional[bool] = False
+    """
+    - 指定是否可以自动获取上层的数据并映射到表单项上
+    - 默认值：false
+    """
+    conditions: Optional[dict] = None
+    """数组的形式包含所有条件的渲染类型，单个数组内的test 为判断条件，数组内的items为符合该条件后渲染的schema"""
+    typeSwitchable: Optional[bool] = None
+    """
+    - 是否可切换条件，配合conditions使用
+    - 默认值：false
+    """
+    strictMode: Optional[bool] = True
+    """默认为严格模式，设置为 false 时，当其他表单项更新是，里面的表单项也可以及时获取，否则不会"""
+    syncFields: Optional[list[str]] = None
+    """配置同步字段。只有 strictMode 为 false 时有效。如果 Combo 层级比较深，底层的获取外层的数据可能不同步。但是给 combo 配置这个属性就能同步下来。输入格式：["os"]"""
+    nullable: Optional[bool]  = False
+    """
+    - 允许为空，如果子表单项里面配置验证器，且又是单条模式。可以允许用户选择清空（不填）
+    - 默认值：false
+    """
+    itemClassName: Optional[str] = None
+    """单组 CSS 类"""
+    itemsWrapperClassName: Optional[str] = None
+    """组合区域 CSS 类"""
+    deleteBtn: Optional[Union[str, Button]] = None
+    """
+    - 只有当removable为 true 时有效; 如果为string则为按钮的文本；如果为Button则根据配置渲染删除按钮。
+    - 默认值：'自定义删除按钮'
+    """
+    addBtn: Optional[Button] = None
+    """
+    - 可新增自定义配置渲染新增按钮，在tabsMode: true下不生效。
+    - 默认值：'自定义新增按钮'
+    """
+    addButtonClassName: Optional[str] = None
+    """新增按钮 CSS 类名"""
+    addButtonText: Optional[str] = None
+    """新增按钮文字"""
